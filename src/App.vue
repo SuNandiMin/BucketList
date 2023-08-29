d43e79d3a38ed93d0f8884d01dec75e1cdeefb52
SuNandiMin <sunandimin@gmail.com>
2023-08-29 13:53:13 +0630

    bucketList

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="">
+  <h1 class="text-white bg-primary text-center p-2 fw-bolder">{{ title }}</h1>
+  <div class="container-lg">
+    <div class="row">
+        <div class="col-5 offset-2 fs-4 mb-2 fw-bold">TASKS</div>
+        <div class="col-2 fs-4 mb-2 fw-bold">Done</div>
+    </div>
+        <div class="row" v-for="(task,index) in tasks" :key="index">
+        <div class="col-5 offset-2 ">{{ task.action }}</div>
+        <div class="col-2 ">{{ task.done }}</div>
+    </div>
+  </div>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data: () => ({
+        title: "Bucket List",
+        tasks: [
+            { action : "Trip to Amsterdam", done: false },
+            { action : "Learning Vue", done: false },
+            { action : "See Taehyung", done: false },
+            { action :"Work as a developer" , done:true}
+        ]
+    }),
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
